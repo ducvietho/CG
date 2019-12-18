7742c458e99065aa3831250deb116dd26763d12c
luongyen <luongyen1508@gmail.com>
2019-12-18 20:28:14 +0700

    update confilic

--- app/Models/NurseProfile.php
+++ app/Models/NurseProfile.php
@@ -9,7 +9,7 @@ class NurseProfile extends Model
 {
 
     protected $table = 'profile_nurse';
-    protected $fillable = ['nationality', 'start_date', 'end_date','start_time','end_time','address','is_certificate','description','start_time','rate','user_login','code_add'];
+    protected $fillable = ['nationality', 'start_date', 'end_date','start_time','end_time','address','is_certificate','description','start_time','rate','user_login','code_add','end_time_1','start_time_1'];
 
     /**
      * Relationship table
