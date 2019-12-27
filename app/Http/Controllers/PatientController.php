18a13b4c732eecf8db4fdb6152307ef76e49f2cf
luongyen <luongyen1508@gmail.com>
2019-12-27 11:31:57 +0700

    update end_time1 patient

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -73,6 +73,7 @@ class PatientController extends Controller
         $end_date = isset($request->end_date) ? $request->end_date : $patient->end_date;
         $end_time = isset($request->end_time) ? $request->end_time : $patient->end_time;
         $start_time = isset($request->start_time) ? $request->start_time : $patient->start_time;
+        $end_time1 =0;
         if ($request->start_time > $request->end_time) {
             $end_time_1 =  $end_time;
             $end_time =  1440;
