be8bfffb78b84899419d78b77197489240de808c
luongyen <luongyen1508@gmail.com>
2020-01-07 13:53:27 +0700

    update role user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -30,7 +30,8 @@ class UserResource extends JsonResource
             'is_register' => $this->is_register,
             'city' => ($this->city != null) ? new CityResource($this->city) : new \stdClass(),
             'district' => ($this->district != null) ? new DistrictResource($this->district) : new \stdClass(),
-            'notification' => count($noti)
+            'notification' => count($noti),
+            'role'=>$this->role
         ];
     }
 }
