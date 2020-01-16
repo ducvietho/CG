ee731f0a1f4aa3728620e3fe53f260f3fbfb47fe
ducvietho1995 <ducvietho1995@gmail.com>
2020-01-16 16:39:23 +0700

    get list request

--- app/Http/Controllers/CMS/CareController.php
+++ app/Http/Controllers/CMS/CareController.php
@@ -9,22 +9,26 @@ use App\Http\Resources\CMS\RequestCMSCollection;
 use App\Models\Care;
 use App\MyConst;
 use App\Traits\ApiResponser;
-use Illuminate\Database\Eloquent\Relations\Concerns\AsPivot;
 use Illuminate\Http\Request;
 
 class CareController extends Controller
 {
     use ApiResponser;
-    public function getRequests(Request $request){
+
+    public function getRequests(Request $request)
+    {
         $query = Care::query();
-        if(isset($request->start_date) && $request->start_date != null){
-            $query =  $query->whereDate('created_at','>=',$request->start_date);
+        if (isset($request->start_date) && $request->start_date != null) {
+            $query = $query->whereDate('created_at', '>=', $request->start_date);
         }
-        if(isset($request->end_date) && $request->end_date != null){
-            $query =  $query->whereDate('created_at','<=',$request->end_date);
+        if (isset($request->end_date) && $request->end_date != null) {
+            $query = $query->whereDate('created_at', '<=', $request->end_date);
         }
-        $collection = $query->orderBy('created_at','DESC')->paginate(MyConst::PAGINATE);
-        return $this->successResponseMessage(new RequestCMSCollection($collection),200,'Get request success');
+        if (isset($request->status) && $request->status >= 0){
+            $query = $query->where('status',$request->status);
+        }
+            $collection = $query->orderBy('created_at', 'DESC')->paginate(MyConst::PAGINATE);
+        return $this->successResponseMessage(new RequestCMSCollection($collection), 200, 'Get request success');
 
 
     }
--- app/Http/Resources/CMS/RequestCMSResource.php
+++ app/Http/Resources/CMS/RequestCMSResource.php
@@ -4,17 +4,52 @@
 namespace App\Http\Resources\CMS;
 
 
+use App\Models\Patient;
+use App\User;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class RequestCMSResource extends JsonResource
 {
     public function toArray($request)
     {
+        $user_patient = Patient::select('name', 'id', 'user_login', 'avatar')->find($this->user_patient);
         return [
             'id_request' => $this->id,
             'created' => strtotime($this->created_at),
-            'type' => $this->type
+            'type' => $this->type,
+            'nurse' => $this->formatNurse($this->user_nurse),
+            'patient' => $this->formatPatient($user_patient),
+            'user_login' => $this->formatUserLogin($this->user_login,$this->type),
+            'status' => $this->status,
 
         ];
+    }
+
+    protected function formatPatient($patient)
+    {
+        return [
+            'id' => $patient->id,
+            'name' => $patient->name,
+        ];
+    }
+
+    protected function formatNurse($nurse_id)
+    {
+        $user = User::select('id', 'phone', 'email', 'name', 'avatar')->find($nurse_id);
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+        ];
+    }
+
+    protected function formatUserLogin($user_login,$type)
+    {
+        $format = new \stdClass();
+        if($type != 1){
+            $user = User::select('id', 'name', 'avatar')->find($user_login);
+            $format->id = $user->id;
+            $format->name = $user->name;
+        }
+        return $format;
     }
 }
